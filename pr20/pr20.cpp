88c8394cc234a0e7ae2718c0351733c4274f120e
группы стационара <st@itstep.lan>
2023-12-24 18:18:19 +0300

    pr

---
+++ pr20/pr20.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main(){
+
+        cout << "   *\n";
+        cout << "  * *\n";
+        cout << " *   *\n";
+        cout << "* * * *\n";
+        cout << "   *\n";
+        cout << "   *\n";
+ //       std::cout << "   *\n";
+        return 0;
+
+
+}
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
